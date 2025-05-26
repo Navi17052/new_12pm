6c85c1edb5477dd3564cd15c7a0e8f1c347441c0
Navi17052 <naveenvadde417@gmail.com>
2025-05-26 12:15:05 +0530

    to see difference between list tuple set in excution

---
+++ emp.py
@@ -0,0 +1,8 @@
+employees=[{'eid':101,'ename':'rahul','esal':'50000'},
+            {'eid':102,'ename':'rohit','esal':'50660'},
+            {'eid':103,'ename':'ramesh','esal':'15000'},
+            {'eid':104,'ename':'raju','esal':'100000'},
+            {'eid':105,'ename':'ravi','esal':'150000'}]
+for emp in employees:
+    
+    print(emp['ename'],"--",emp['esal'])
---
+++ list.py
@@ -0,0 +1,14 @@
+eids=[101,102,103,101]
+for eid in eids:
+    print(eid)
+
+
+ #tuple program
+eids=(101,102,103,101)
+for eid in eids:
+    print(eid)
+
+    # set program
+eids={101,102,103,101}
+for eid in eids:
+    print(eid)    

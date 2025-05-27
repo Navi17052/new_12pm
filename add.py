c01de8cf37a1c839c49c85f700803b41ad9ea472
Navi17052 <naveenvadde417@gmail.com>
2025-05-27 13:01:54 +0530

    operators in python

---
+++ add.py
@@ -0,0 +1,7 @@
+a=10
+b=29
+print(a+b)
+
+a=10
+b='rahul'
+print(a+b)
---
+++ logical.py
@@ -0,0 +1,11 @@
+a=10
+b=2000
+print(a<=200)
+print(1000>=b)
+print(a<=200 and 1000>=b)
+
+a=10
+b=2000
+print(a<=200)
+print(1000>=b)
+print(a<=200 or 1000>=b)
